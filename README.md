b6dba1f7c95d6918b0b849286c61bf790ff0a790
Casper Klenz-Kitenge <hi@cabgfx.com>
2014-06-11 22:21:38 +0200

    Initial setup

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
-WPBP Theme
+# WPBP Theme
 ==========
 
-Bare-bones theme configuration using SCSS for Roots, a WordPress starter theme based on HTML5 Boilerplate & Bootstrap from Twitter.
+Bare-bones theme configuration using SCSS for [Roots](http://roots.io/), a WordPress starter theme based on [HTML5 Boilerplate](http://html5boilerplate.com/) & [Bootstrap](http://getbootstrap.com/) that will help you make better themes.
 
-Currently being developed as a pre-release, so things _will_ break.
-Check back often, expected release is late August, 2013.
+## Deviations from default Roots theme:
+
+* SCSS structure/organization - a boilerplate file structure, expecting certain styles to go in certain files. Also includes a few default styles, such as legibility optimizations and an alternative `@mixin` for Retina/HiDPI background images.
+* Only a subset of Bootstrap CSS included by default. To add more, uncomment the `@import`'s you need in `assets/scss/_bootstrap.scss`. (All Bootstrap files are kept pristine and update-friendly.)
+* No Bootstrap JS included by default, and `assets/js/main.js` only includes the `Roots` namespace + DOM-based Routing.
+* Bootstrap top navbar off by default.
+* Bootstrap thumbnails for galleries off by default.
+* Nice search disabled, preferring the WP default of using the /?s= parameter, as this makes it easier to set up Google Analytics' site search tracking.
+* Google Analytics tracking code is only included if `WPBP_ENV == 'production'` and no user is logged in. (Note: the `WPBP_ENV` constant is defined by the [WordPress Boilerplate config package](https://github.com/cabgfx/wpbp-config), but this theme does not require it.)
+* All theme activation options are off by default, to prevent “accidents” when installing the theme on sites with existing content.
+* No “You're using an outdated browser…” message for IE8 and below.
--- lib/scripts.php
+++ lib/scripts.php
@@ -3,23 +3,22 @@
  * Enqueue scripts and stylesheets
  *
  * Enqueue stylesheets in the following order:
- * 1. /theme/assets/css/theme.css (compiled version of /theme/assets/scss/theme.scss)
+ * 1. /theme/assets/css/main.min.css
  *
  * Enqueue scripts in the following order:
- * 1. jquery-1.10.1.min.js via Google CDN
- * 2. /theme/assets/js/vendor/modernizr-2.6.2.min.js
- * 3. /theme/assets/js/plugins.js (in footer)
- * 4. /theme/assets/js/main.js    (in footer)
+ * 1. jquery-1.11.0.min.js via Google CDN
+ * 2. /theme/assets/js/vendor/modernizr-2.7.0.min.js
+ * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_theme', get_template_directory_uri() . '/assets/css/theme.css', false, null);
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/theme.css', false, '64c2848549e90cef42796141ccce4c3e');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
   // It's kept in the header instead of footer to avoid conflicts with plugins.
   if (!is_admin() && current_theme_supports('jquery-cdn')) {
     wp_deregister_script('jquery');
-    wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js', false, null, false);
+    wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js', array(), null, false);
     add_filter('script_loader_src', 'roots_jquery_local_fallback', 10, 2);
   }
 
@@ -27,22 +26,20 @@ function roots_scripts() {
     wp_enqueue_script('comment-reply');
   }
 
-  wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.6.2.min.js', false, null, false);
-  wp_register_script('roots_plugins', get_template_directory_uri() . '/assets/js/plugins.js', false, null, true);
-  wp_register_script('roots_main', get_template_directory_uri() . '/assets/js/main.js', false, null, true);
-  wp_enqueue_script('jquery');
+  wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.7.0.min.js', array(), null, false);
+  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/main.js', array(), '0fc6af96786d8f267c8686338a34cd38', true);
   wp_enqueue_script('modernizr');
-  wp_enqueue_script('roots_plugins');
-  wp_enqueue_script('roots_main');
+  wp_enqueue_script('jquery');
+  wp_enqueue_script('roots_scripts');
 }
 add_action('wp_enqueue_scripts', 'roots_scripts', 100);
 
 // http://wordpress.stackexchange.com/a/12450
-function roots_jquery_local_fallback($src, $handle) {
+function roots_jquery_local_fallback($src, $handle = null) {
   static $add_jquery_fallback = false;
 
   if ($add_jquery_fallback) {
-    echo '<script>window.jQuery || document.write(\'<script src="' . get_template_directory_uri() . '/assets/js/vendor/jquery-1.10.1.min.js"><\/script>\')</script>' . "\n";
+    echo '<script>window.jQuery || document.write(\'<script src="' . get_template_directory_uri() . '/assets/js/vendor/jquery-1.11.0.min.js"><\/script>\')</script>' . "\n";
     $add_jquery_fallback = false;
   }
 
@@ -52,6 +49,7 @@ function roots_jquery_local_fallback($src, $handle) {
 
   return $src;
 }
+add_action('wp_head', 'roots_jquery_local_fallback');
 
 function roots_google_analytics() { ?>
 <script>
@@ -64,6 +62,6 @@ function roots_google_analytics() { ?>
 </script>
 
 <?php }
-if (GOOGLE_ANALYTICS_ID && WPBP_ENV == 'production' && !(is_user_logged_in())) {
+if (GOOGLE_ANALYTICS_ID && (defined('WPBP_ENV') && (WPBP_ENV == 'production')) && !(is_user_logged_in())) {
   add_action('wp_footer', 'roots_google_analytics', 20);
 }
