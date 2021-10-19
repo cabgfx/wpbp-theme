98a50596800c59881e46e94697f66e9c6413ce99
Casper Klenz-Kitenge <hi@cabgfx.com>
2021-10-19 16:59:40 +0200

    Disable <style> tag cleanup, temp. fix
    Pending resolve in #4

--- lib/cleanup.php
+++ lib/cleanup.php
@@ -95,13 +95,13 @@ add_filter('wp_title', 'roots_wp_title', 10);
 /**
  * Clean up output of stylesheet <link> tags
  */
-function roots_clean_style_tag($input) {
-  preg_match_all("!<link rel='stylesheet'\s?(id='[^']+')?\s+href='(.*)' type='text/css' media='(.*)' />!", $input, $matches);
-  // Only display media if it is meaningful
-  $media = $matches[3][0] !== '' && $matches[3][0] !== 'all' ? ' media="' . $matches[3][0] . '"' : '';
-  return '<link rel="stylesheet" href="' . $matches[2][0] . '"' . $media . '>' . "\n";
-}
-add_filter('style_loader_tag', 'roots_clean_style_tag');
+// function roots_clean_style_tag($input) {
+//   preg_match_all("!<link rel='stylesheet'\s?(id='[^']+')?\s+href='(.*)' type='text/css' media='(.*)' />!", $input, $matches);
+//   // Only display media if it is meaningful
+//   $media = $matches[3][0] !== '' && $matches[3][0] !== 'all' ? ' media="' . $matches[3][0] . '"' : '';
+//   return '<link rel="stylesheet" href="' . $matches[2][0] . '"' . $media . '>' . "\n";
+// }
+// add_filter('style_loader_tag', 'roots_clean_style_tag');
 
 /**
  * Add and remove body_class() classes
