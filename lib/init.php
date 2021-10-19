11c08cc7ab32787027b788eade057e403fda6e4c
Casper Klenz-Kitenge <hi@cabgfx.com>
2021-10-19 16:53:09 +0200

    Use theme-supports for title tag and feed links

--- lib/init.php
+++ lib/init.php
@@ -19,5 +19,7 @@ function roots_setup() {
   // Add post formats (http://codex.wordpress.org/Post_Formats)
   // add_theme_support('post-formats', array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat'));
 
+  add_theme_support( 'title-tag' );
+  add_theme_support( 'automatic-feed-links' );
 }
 add_action('after_setup_theme', 'roots_setup');
