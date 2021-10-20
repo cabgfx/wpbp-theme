0eaee2d9bb77f14af6ce1008a4b7ccbcedc96fb1
Casper Klenz-Kitenge <hi@cabgfx.com>
2021-10-20 12:40:38 +0200

    Load child theme stylesheet, if available

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/theme.css', false, '64c2848549e90cef42796141ccce4c3e');
+  wp_enqueue_style('roots_main', get_stylesheet_directory_uri() . '/assets/css/theme.css', false, '64c2848549e90cef42796141ccce4c3e');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
@@ -27,7 +27,7 @@ function roots_scripts() {
   }
 
   wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-3.5.0.min.js', array(), null, false);
-  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/main.js', array(), '0fc6af96786d8f267c8686338a34cd38', true);
+  wp_register_script('roots_scripts', get_stylesheet_directory_uri() . '/assets/js/main.js', array(), '0fc6af96786d8f267c8686338a34cd38', true);
   wp_register_script('bs-popper', get_template_directory_uri() . '/assets/js/vendor/popper.min.js', array(), '0fc6af96786d8f267c8686338a34cd38', true);
   wp_register_script('bs-util', get_template_directory_uri() . '/assets/js/vendor/bootstrap/util.js', array(), '0fc6af96786d8f267c8686338a34cd38', true);
   wp_register_script('bs-collapse', get_template_directory_uri() . '/assets/js/vendor/bootstrap/collapse.js', array(), '0fc6af96786d8f267c8686338a34cd38', true);
