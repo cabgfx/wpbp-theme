40c11ef93458bd1a7b125c9b3cfb925c38efb225
Casper Klenz-Kitenge <hi@cabgfx.com>
2021-11-29 13:55:31 +0100

    Remove Helo.app config in favor separate config plugin

--- lib/custom.php
+++ lib/custom.php
@@ -3,18 +3,3 @@
  * Custom functions
  */
 
-// Helo.app local mail catcher
-function helo($phpmailer)
-{
-  $phpmailer->isSMTP();
-  $phpmailer->Host = '127.0.0.1';
-  $phpmailer->SMTPAuth = true;
-  $phpmailer->Port = 2525;
-  $phpmailer->Username = 'Mailbox-Name';
-  $phpmailer->Password = '';
-}
-
-if (defined('WPBP_ENV') && (WPBP_ENV == 'development')) {
-  add_action('phpmailer_init', 'helo');
-}
-
