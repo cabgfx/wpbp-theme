059a5336b02285d23e0086d72a6f59d8e7225aab
Casper Klenz-Kitenge <hi@cabgfx.com>
2024-03-08 20:03:38 +0100

    Remove Google Analytics script
    🖕

--- lib/scripts.php
+++ lib/scripts.php
@@ -59,18 +59,3 @@ function roots_jquery_local_fallback($src, $handle = null) {
   return $src;
 }
 add_action('wp_head', 'roots_jquery_local_fallback');
-
-function roots_google_analytics() { ?>
-<script>
-  (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-  function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-  e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-  e.src='//www.google-analytics.com/analytics.js';
-  r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-  ga('create','<?php echo GOOGLE_ANALYTICS_ID; ?>');ga('send','pageview');
-</script>
-
-<?php }
-if (GOOGLE_ANALYTICS_ID && (defined('WPBP_ENV') && (WPBP_ENV == 'production')) && !(is_user_logged_in())) {
-  add_action('wp_footer', 'roots_google_analytics', 20);
-}
